de7212b65e7799981da10960acfdc70df69430ac
kolomyya <andriyzn4@gmail.com>
2018-12-22 23:33:34 +0000

    confguration folder added

---
+++ configuration/prod/prod.tfvars
@@ -0,0 +1,3 @@
+instance_type             =  "t2.medium"
+ami                       =  "ami-0274e11dced17bb5b"
+region                    =  "eu-west-2"
--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "${var.region}"
 }
 terraform{
   backend"s3"{
     bucket = "eu-west-1-state-andrew"
-    key = "mystate"
+    key = "mystate-cluster"
     region = "eu-west-1"
  
  } 
